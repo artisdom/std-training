7cda2bc5803f4ebd784a5017f6d04d03b9c06840
Ted Feng <Xigang.Feng@gallagher.com>
2023-12-03 18:32:27 +1300

    Part 1: Reading Temperature & Humidity of I2C workshop.

--- advanced/i2c-sensor-reading/src/main.rs
+++ advanced/i2c-sensor-reading/src/main.rs
@@ -19,15 +19,30 @@ fn main() -> Result<()> {
     let peripherals = Peripherals::take().unwrap();
 
     // 1. Instantiate the SDA and SCL pins, correct pins are in the training material.
+    let sda = peripherals.pins.gpio10;
+    let scl = peripherals.pins.gpio8;
 
     // 2. Instantiate the i2c peripheral,I2cDriver, using a I2cConfig of 400kHz
+    let config = I2cConfig::new().baudrate(400.kHz().into());
+    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config)?;
 
     // 3. Create an instance of the SHTC3 sensor.
+    let mut sht = shtcx::shtc3(i2c);
 
     // 4. Read and print the sensor's device ID.
+    let device_id = sht.device_identifier().unwrap();
+    println!("Device ID SHTC3: {:#02x}", device_id);
 
     loop {
         // 5. This loop initiates measurements, reads values and prints humidity in % and Temperature in °C.
+        sht.start_measurement(PowerMode::NormalMode).unwrap();
+        FreeRtos.delay_ms(100u32);
+        let measurement = sht.get_measurement_result().unwrap();
+
+        println!("TEMP: {:.2} °C | HUM: {:.2} %",
+            measurement.temperature.as_degrees_celsius(),
+            measurement.humidity.as_percent());
+
         FreeRtos.delay_ms(500u32);
     }
 }
